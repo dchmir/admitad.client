7cf7e21ac9f0007026d663ab3ef46a35b5578e5c
Dmitry <dmitrychmir1975@gmail.com>
2018-01-22 13:43:00 +0300

    Add Solution

---
+++ Admitad/Admitad.Client/DTOs/BaseDto.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------------------------------------
+// Created date: 2018 01 22 13:35
+// 
+// Solution name: Admitad
+// Project name: Admitad.Client
+// File name: BaseDto.cs
+// 
+// Created by: Дмитрий Чмир
+// -----------------------------------------------------------------------------------------------------
+namespace Admitad.Client.DTOs
+{
+    public abstract class BaseDto
+    {
+        
+    }
+}
